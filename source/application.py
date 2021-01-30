06059745220ecf432a9738ffb6f08d8d7d89f82e
unknown <Suleiman.ahmed2001@gmail.com>
2021-01-30 11:28:21 +0400

    update version

--- source/application.py
+++ source/application.py
@@ -1,6 +1,6 @@
 # a module to hold application constants to be not re written everytime
 
 name = "accessible youtube downloader pro"
-version = "1.0"
+version = "1.1"
 author = "سليمان القسيمي"
 describtion = "يهدف هذا البرنامج إلى تقديم أيسر السبل أمام المستخدم الكفيف للتصفح والتنزيل من خدمة استضافة الفيديوهات youtube"

1ff726f47307de10c6d84c3532f1997e591936ea
unknown <Suleiman.ahmed2001@gmail.com>
2021-01-30 16:50:36 +0400

    move the time_formatting method to the utiles modules and import it here

--- source/youtube_browser/search_handler.py
+++ source/youtube_browser/search_handler.py
@@ -1,6 +1,6 @@
 from youtubesearchpython import VideosSearch, CustomSearch
 from dialogs.search_dialog import SearchDialog
-
+from utiles import time_formatting
 
 
 
@@ -57,45 +57,9 @@ class Search:
 			return _("عدد المشاهدات {}").format(data)
 		else:
 			return _("بث مباشر")
-	def time_formatting(self, t):
-		t = t.split(":")
-		t = [int(i) for i in t]
-		def minute(m):
-			if m == 1:
-				return _("دقيقة واحدة")
-			elif m == 2:
-				return _("دقيقتان")
-			elif m >=3 and m <=10:
-				return _("{} دقائق").format(m)
-			else:
-				return _("{} دقيقة").format(m)
-		def second(s):
-			if s == 1:
-				return _("ثانية")
-			elif s == 2:
-				return _("ثانيتين")
-			elif s >= 3 and s <= 10:
-				return _("{} ثواني").format(s)
-			else:
-				return _("{} ثانية").format(s)
-		def hour(h):
-			if h == 1:
-				return _("ساعة")
-			elif h == 2:
-				return _("ساعتان")
-			elif h >= 3 and h <=10:
-				return _("{} ساعات").format(h)
-			else:
-				return _("{} ساعة").format(h)
-		if len(t) == 1:
-			return second(t[0])
-		elif len(t) == 2:
-			return _("{} و{}").format(minute(t[0]), second(t[1]))
-		elif len(t) == 3:
-			return _("{} و{} و{}").format(hour(t[0]), minute(t[1]), second(t[2]))
 
 	def get_duration(self, data): # get the duration of the video
 		if data is not None:
-			return _(" المدة: {},").format(self.time_formatting(data))
+			return _(" المدة: {},").format(time_formatting(data))
 		else:
 			return ""

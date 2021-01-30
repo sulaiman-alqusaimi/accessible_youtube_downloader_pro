981f8e2c7ca3cd3f8ce3db40380d59da57e60a26
unknown <Suleiman.ahmed2001@gmail.com>
2021-01-30 08:05:49 +0400

    added the English translation for video duration

--- source/youtube_browser/search_handler.py
+++ source/youtube_browser/search_handler.py
@@ -65,37 +65,37 @@ class Search:
 		t = [int(i) for i in t]
 		def minute(m):
 			if m == 1:
-				return "دقيقة واحدة"
+				return _("دقيقة واحدة")
 			elif m == 2:
-				return "دقيقتان"
+				return _("دقيقتان")
 			elif m >=3 and m <=10:
-				return "{} دقائق".format(m)
+				return _("{} دقائق").format(m)
 			else:
-				return "{} دقيقة".format(m)
+				return _("{} دقيقة").format(m)
 		def second(s):
 			if s == 1:
-				return "ثانية"
+				return _("ثانية")
 			elif s == 2:
-				return "ثانيتين"
+				return _("ثانيتين")
 			elif s >= 3 and s <= 10:
-				return "{} ثواني".format(s)
+				return _("{} ثواني").format(s)
 			else:
-				return "{} ثانية".format(s)
+				return _("{} ثانية").format(s)
 		def hour(h):
 			if h == 1:
-				return "ساعة"
+				return _("ساعة")
 			elif h == 2:
-				return "ساعتان"
+				return _("ساعتان")
 			elif h >= 3 and h <=10:
-				return "{} ساعات".format(h)
+				return _("{} ساعات").format(h)
 			else:
-				return "{} ساعة".format(h)
+				return _("{} ساعة").format(h)
 		if len(t) == 1:
 			return second(t[0])
 		elif len(t) == 2:
-			return "{} و{}".format(minute(t[0]), second(t[1]))
+			return _("{} و{}").format(minute(t[0]), second(t[1]))
 		elif len(t) == 3:
-			return "{} و{} و{}".format(hour(t[0]), minute(t[1]), second(t[2]))
+			return _("{} و{} و{}").format(hour(t[0]), minute(t[1]), second(t[2]))
 
 	def get_duration(self, data): # get the duration of the video
 		if data is not None:

b8b07ef79e976f8e125f6a36d989fe9ae22970de
Sulaiman Al QUsaimi <Suleiman.ahmed2001@gmail.com>
2021-05-24 18:04:57 +0400

    add the ability to detect the link from the clipboard and some few ui changes

--- source/dialogs/link_dlg.py
+++ source/dialogs/link_dlg.py
@@ -1,4 +1,6 @@
 import wx
+import pyperclip
+from utiles import youtube_regexp
 
 
 
@@ -11,19 +13,40 @@ class LinkDlg(wx.Dialog):
 		lbl = wx.StaticText(panel, -1, _("رابط المقطع"))
 		self.link = wx.TextCtrl(panel, -1, value="")
 		self.mode = wx.RadioBox(panel, -1, _("التشغيل ك: "), choices=[_("مقطع فيديو"), _("مقطع صوتي")])
-		okButton = wx.Button(panel, wx.ID_OK, _("موافق"))
-		okButton.SetDefault()
+		self.okButton = wx.Button(panel, wx.ID_OK, _("تشغيل"))
+		self.okButton.SetDefault()
+		cancelButton = wx.Button(panel, wx.ID_CANCEL, _("إغلاق"))
 		sizer1 = wx.BoxSizer(wx.HORIZONTAL)
 		sizer1.Add(lbl, 1)
 		sizer1.Add(self.link, 1)
 		sizer.Add(sizer1, 1, wx.EXPAND)
 		sizer.Add(self.mode, 1, wx.ALL)
-		sizer.Add(okButton, 1)
+		okCancelSizer = wx.StdDialogButtonSizer()
+		for btn in (self.okButton, cancelButton):
+			okCancelSizer.Add(btn)
+		okCancelSizer.Realize()
+		sizer.Add(okCancelSizer, 1)
 		panel.SetSizer(sizer)
-		okButton.Bind(wx.EVT_BUTTON, self.onOk)
+		self.okButton.Bind(wx.EVT_BUTTON, self.onOk)
+		self.link.Bind(wx.EVT_TEXT, self.onText)
+		self.detectFromClipboard()
 		self.ShowModal()
 	def onOk(self, event):
 		link = self.link.Value
 		audio = True if self.mode.Selection == 1 else False
 		self.data = {"link": link, "audio": audio}
 		self.Destroy()
+	def isYoutubeLink(self, value):
+		match = youtube_regexp(value)
+		return match is not None
+
+	def onText(self, event):
+		self.okButton.Enabled = self.isYoutubeLink(self.link.Value)
+
+	def detectFromClipboard(self):
+		clip_content = pyperclip.paste() # get the clipboard content
+		if self.isYoutubeLink(clip_content):
+			self.link.Value = clip_content 
+			self.okButton.Enabled = True
+		else:
+			self.okButton.Enabled = False

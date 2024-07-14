75cdb296778a85d7ab317a54e020d8e25d914064
Sulaiman Al QUsaimi <Suleiman.ahmed2001@gmail.com>
2024-07-14 11:05:59 +0400

    fix live videos

--- source/pafy/backend_youtube_dl.py
+++ source/pafy/backend_youtube_dl.py
@@ -48,7 +48,7 @@ class YtdlPafy(BasePafy):
         self._title = self._ydl_info['title']
         self._author = self._ydl_info['uploader']
         self._rating = self._ydl_info['average_rating']
-        self._length = self._ydl_info['duration']
+        self._length = self._ydl_info.get('duration', 0)
         self._viewcount = self._ydl_info.get('view_count', 0)
         self._likes = self._ydl_info.get('like_count', 0)
         self._dislikes = self._ydl_info.get('dislike_count', 0)
